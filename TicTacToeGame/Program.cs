e8c335f42a22c04614aedd928b2e769d3511b433
VSRUJANA <srujana.valavala@gmail.com>
2020-10-05 14:06:12 +0530

    Add project files.

---
+++ TicTacToeGame/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TicTacToeGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to TicTacToe Game!");
+            var t = new TicTacToe();
+            t.CreateBoard();
+        }
+    }
+}
---
+++ TicTacToeGame/TicTacToe.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToeGame
+{
+    class TicTacToe
+    {
+        public void CreateBoard()
+        {
+            char[] board = new char[10];
+            board[1] = ' ';
+            board[2] = ' ';
+            board[3] = ' ';
+            board[4] = ' ';
+            board[5] = ' ';
+            board[6] = ' ';
+            board[7] = ' ';
+            board[8] = ' ';
+            board[9] = ' ';
+            board[10] = ' ';
+        }
+    }
+}
+
+
+
